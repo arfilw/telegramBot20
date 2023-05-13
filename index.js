126796cc8441c8b6345865888e6444fa5668beb0
arfilw <133174467+arfilw@users.noreply.github.com>
2023-05-13 17:34:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ const sendVids = async function (nVids, myAd) {
 			}
 
 			let extraSend = {
-				caption: vid.caption,
+				caption: "",
 				parse_mode: "markdown",
 				disable_web_page_preview: "true",
 			}
